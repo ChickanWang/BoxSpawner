93764c41ed05059b12c90c30835b8c66b8105e7e
Stephen Wang <stephenwang1234567890@gmail.com>
2021-03-20 19:56:20 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Form from'./Form.js'
+
+function App() {
+  return (
+    <div className="center">
+      <h1>Box Spawner</h1>
+      <body>Set the width, height and color of your box to see it come to life!</body> 
+      <br></br>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { ChromePicker } from 'react-color'
+import './App.css'
+
+function Form() {
+    const [dimensions, setDimensions] = useState({width: 100, height: 100});
+    const [color, setColor] = useState({rgb: {r: 69, g: 255, b: 255}});
+    const [box, setBox] = useState(false);
+
+    var rgb = "rgb(" + color.rgb.r + ", " + color.rgb.g + ", " + color.rgb.b + ")"
+
+    const changeDimensions = (e) => {
+
+        setDimensions({...dimensions, [e.target.name] : e.target.value})
+    }
+
+    const changeColor = (color, e) => {
+        setColor({
+            ...color,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setBox(!box);
+    }
+
+    return (
+    <div> 
+        {box ? 
+
+        <div>
+            <div style={{width: parseInt(dimensions.width), height: parseInt(dimensions.height), background: rgb}}></div>
+            <br></br>
+            <button onClick={handleSubmit} type="submit" class="btn btn-primary" >Create Another Box!</button>
+        </div> :
+
+        <form onSubmit={handleSubmit}>
+            <h3>Box Settings:</h3>
+            <div class="col-md-8 col-s-12">
+                <div class="form-group">
+                    <label for="BoxWidth">Box Width (Pixels)</label>
+                    <input name="width" type="number" min="0" max="9999" class="form-control" id="BoxWidth" placeholder="Enter width" value={dimensions.width} onChange={changeDimensions}></input>
+                </div>
+                <div class="form-group">
+                    <label for="BoxHeight">Box Height (Pixels)</label>
+                    <input name="height" type="number" min="0" max="9999" class="form-control" id="BoxHeight" placeholder="Enter height" value={dimensions.height} onChange={changeDimensions}></input>
+                </div>
+                <div class="form-group col-2 input-lg">
+                    <label for="Color">Color</label>
+                    <ChromePicker color={color} onChange={changeColor}/>
+                </div>
+                <br></br>
+                <button type="submit" class="btn btn-primary" >Create Box</button>
+            </div>
+        </form>}
+    </div>
+    );
+}
+
+export default Form;
